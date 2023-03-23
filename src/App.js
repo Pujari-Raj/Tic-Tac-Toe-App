0251c736a2b42c7f514d3c649ed484f66ed5210b
Pujari-Raj <basvarajpujari607@gmail.com>
2023-03-23 13:13:19 +0530

    added form valdiation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
 
+
   <div className='App'>
     <PlayersForm  />
   </div>  
--- src/Components/PlayersForm.jsx
+++ src/Components/PlayersForm.jsx
@@ -13,9 +13,10 @@ const PlayersForm = () => {
   const[isplayerformvisible, setIsPlayerFormVisible] = useState(true);
 
   const handleSubmit = (event) => {
-    // alert('form data received');
+    alert('form data received');
     event.preventDefault();
     setIsPlayerFormVisible(false);
+
   };
 
   const handleChange = (event) => {
@@ -42,7 +43,7 @@ const PlayersForm = () => {
                 <input
                   className="border-b-2 border-x-gray-400  flex-1 py-2 placeholder-gray-500 outline-none focus:border-gray-600"
                   name="player1_name" placeholder="Enter circle/cross" type="text" id="player1_name" 
-                 value={playersname?.player1} onChange={handleChange}/>
+                 value={playersname?.player1} onChange={handleChange} required />
           
               </div>
               <div className="flex flex-col md:flex-row md:items-center justify-center mb-16">
@@ -52,7 +53,7 @@ const PlayersForm = () => {
                 <input
                   className="border-b-2 border-x-gray-400 flex-1 py-2 placeholder-gray-500 outline-none focus:border-gray-600"
                   name="player2_name" placeholder="Enter circle/cross" type="text" id="player2_name"
-                  value={playersname?.player2} onChange={handleChange}/>
+                  value={playersname?.player2} onChange={handleChange} required />
               </div>
               <div className="text-center">
                 <button className="py-3 px-2 md:px-4 bg-[#68B984] hover:bg-[#3C6255] ease-in-out duration-300 text-white font-normal rounded">
